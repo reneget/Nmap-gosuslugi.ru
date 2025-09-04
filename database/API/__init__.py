a87d2b30d1462c82ae640f241f163b328c0259cd
pivan-ubuntu <popov-id06@mail.ru>
2025-09-04 14:28:19 +0700

    adjustments in the database

---
+++ database/API/__init__.py
@@ -0,0 +1,1 @@
+from .users_route import user_router
---
+++ database/API/users_route/route.py
@@ -0,0 +1,165 @@
+from fastapi import APIRouter, status, HTTPException, Depends
+from sqlalchemy.orm import Session
+
+from . import pydantic_models as pd_md 
+from DataBase.core.db_connection import get_db 
+from DataBase.repositories import UserRepo
+from ..functions import Functions_API as Func_API
+
+import logging
+
+user_logger = logging.getLogger(__name__)
+
+user_router = APIRouter(
+    prefix='/user',
+    tags=['user']
+)
+
+@user_router.post('/create/user')
+async def create_user_route(user: pd_md.User_create, db: Session = Depends(get_db)):
+    try:
+        user_logger.info('Request to create a new employee')
+        created_user = UserRepo(db).create_user(**user.__dict__)
+        user_logger.debug(f'{repr(created_user)}')
+        user_logger.info('New employee created')
+        user_logger.info(f'Employee {created_user.full_name} was issued a new token')
+
+        return pd_md.User(**created_user.__dict__)
+    except:
+        user_logger.error('An error occurred while creating the employee', exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+
+@user_router.get('/get/user/{user_id}')
+async def get_user_by_id_api(user_id: int, db: Session = Depends(get_db)):
+    try:
+        user_logger.info(f'Request to get user: user_id={user_id}')
+        user = UserRepo(db).get_user_by_id(user_id)
+        user_logger.info('User received')
+        user_logger.debug(repr(user))
+
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='User not found'
+            )
+
+        return pd_md.User(**user.__dict__)
+    except HTTPException:
+        user_logger.error('Error getting user', exc_info=True)
+        raise
+    except:
+        user_logger.error('Error getting user', exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+    
+@user_router.get('/get/unique_key/user/{user_id}')
+async def get_user_by_id_api(user_id: int, db: Session = Depends(get_db)):
+    try:
+        user_logger.info(f'Request to get user unique key: user_id={user_id}')
+        key = UserRepo(db).get_user_unique_key(user_id)
+        user_logger.info('User unique key received')
+
+        if key is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='User not found'
+            )
+
+        return {f'key: {key}'}
+    except HTTPException:
+        user_logger.error('Error getting user unique key', exc_info=True)
+        raise
+    except:
+        user_logger.error('Error getting user unique key', exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+
+@user_router.get('/get/all/users')
+async def get_user_by_id_api(db: Session = Depends(get_db)):
+    try:
+        user_logger.info('Request to get all users')
+        list_users = UserRepo(db).get_all_users()
+        user_logger.info('All users received')
+
+        new_list = Func_API.convert_list_users(list_users)
+        user_logger.info('Users converted to pydantic model')
+
+        return new_list
+    except:
+        user_logger.error('Error getting all users', exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+
+@user_router.put('/update/user/{user_id}')
+async def update_user_api(user_id: int, user: pd_md.User_update, db: Session = Depends(get_db)):
+    try:
+        user_logger.info(f'User update request: user_id={user_id}')
+        new_user = UserRepo(db).update_user(user_id, **user.__dict__)
+        user_logger.info('User updated')
+
+        if new_user is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='User not found'
+            )
+
+        return  pd_md.User(**new_user.__dict__)
+    except HTTPException:
+        user_logger.error('Error updating user', exc_info=True)
+        raise
+    except:
+        user_logger.error('Error updating user', exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+    
+@user_router.put('update/user/unique_key/{user_id}')
+async def update_user_key_api(user_id: int, new_key: str, db: Session = Depends(get_db)):
+    try:
+        user_logger.info(f'Request to update key: user_id={user_id}')
+        new_user = UserRepo(db).update_user_key(user_id, new_key)
+        user_logger.info('Key updated')
+
+        if new_user is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='User not found'
+            )
+        return pd_md.User(**new_user.__dict__)
+    except HTTPException:
+        user_logger.error('Error updating user key', exc_info=True)
+        raise
+    except:
+        user_logger.error('Error updating user key', exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+    
+@user_router.delete('/delete/user/{user_id}')
+async def delete_user_api(user_id: int, db: Session = Depends(get_db)):
+    try:
+        user_logger.info(f'Request to delete user: user_id={user_id}')
+        user = UserRepo(db).delete_user(user_id)
+        user_logger.info('User deleted')
+
+        if user is None:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail='User not found'
+            )
+        return pd_md.User(**user.__dict__)
+    except HTTPException:
+        user_logger.error('Error deleting user', exc_info=True)
+        raise
+    except:
+        user_logger.error('Error deleting user', exc_info=True)
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR
+        )
+
+
