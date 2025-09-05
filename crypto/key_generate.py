b01774802f413ecdd38e3a251e2d96564426e82f
Ctranikinn <afanasevba70@gmail.com>
2025-09-05 10:11:00 +0700

    ubral commenty

--- crypto/key_generate.py
+++ crypto/key_generate.py
@@ -8,9 +8,9 @@ def generate_entropy(length) -> str:
 
 def generate_key(shared_secret: str, entropy: str) -> str:
     """
-    Генерирует ключ на основе секрета и счётчика.
+    Генерирует ключ на основе секрета и "энтропии".
     :param shared_secret: секрет.
-    :param entropy: счётчик (кол-во считываний).
+    :param entropy: "энтропия"
     :return: Строковый ключ.
     """
     # Конвертируем счётчик в байты (big-endian, 8 байт)
@@ -19,9 +19,10 @@ def generate_key(shared_secret: str, entropy: str) -> str:
     # Кодируем секрет в байты
     secret_bytes = shared_secret.encode('utf-8')
 
-    # Вычисляем HMAC-SHA512 от счётчика с использованием секрета
+    # Вычисляем HMAC-SHA512 от энтропии с использованием секрета
     hmac_hash = hmac.new(secret_bytes, entropy_bytes, hashlib.sha512).digest()
     return base64.b64encode(hmac_hash).decode('utf-8').rstrip('='), entropy
 
 shared_secret = str(open('crypto/secret.txt', 'r').read())
 key_base64 = generate_key(shared_secret, entropy=generate_entropy(16))[0]
+print(key_base64)
