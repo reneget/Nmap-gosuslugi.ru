a96851d877ff4817a6e162ac826557bfb66cf72b
Ctranikinn <afanasevba70@gmail.com>
2025-09-05 10:54:44 +0700

    card emulator+

--- emulator/reader.py
+++ emulator/reader.py
@@ -1,23 +1,8 @@
 #!/usr/bin/env python3
-import hmac
-import hashlib
-import base64
+from crypto import generate_key, generate_entropy
 import os
 
 SECRET_FILE = 'secret_key.txt'
-COUNTER_FILE = 'counter_reader.txt'
-
-def load_counter():
-    if os.path.exists(COUNTER_FILE):
-        try:
-            return int(open(COUNTER_FILE).read().strip())
-        except ValueError:
-            return 0
-    return 0
-
-def save_counter(counter):
-    with open(COUNTER_FILE, 'w') as f:
-        f.write(str(counter))
 
 def load_secret_key():
     if not os.path.exists(SECRET_FILE):
@@ -32,33 +17,16 @@ def load_secret_key():
     except:
         return None
 
-def generate_entropy(shared_secret: str, counter: int) -> bytes:
-    return hmac.new(
-        shared_secret.encode('utf-8'), 
-        counter.to_bytes(8, 'big'), 
-        hashlib.sha512
-    ).digest()
-
-def generate_key(shared_secret: str, counter: int) -> str:
-    entropy_bytes = generate_entropy(shared_secret, counter)
-    hmac_hash = hmac.new(
-        shared_secret.encode('utf-8'), 
-        entropy_bytes, 
-        hashlib.sha512
-    ).digest()
-    return base64.b64encode(hmac_hash).decode('utf-8').rstrip('=')
 
 def verify_key(input_key: str):
     secret_key = load_secret_key()
     if not secret_key:
         return False
     
-    current_counter = load_counter()
-    expected_key = generate_key(secret_key, current_counter)
+    expected_key = generate_key(secret_key, entropy)
     
     if input_key == expected_key:
-        new_counter = current_counter + 1
-        save_counter(new_counter)
+        
         return True
     
     return False
